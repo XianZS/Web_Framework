41d4676e05991342284b642992fe1dc7964400e7
XianZS <xianzhisen_yang@outlook.com>
2026-01-19 15:02:34 +0800

    2

--- FastAPI_Learn/9_fastapi_depend_about_child.py
+++ FastAPI_Learn/9_fastapi_depend_about_child.py
@@ -1,31 +1,32 @@
 from fastapi import Depends, FastAPI
-from typing import Union
+from fastapi import Form
+
 
 app = FastAPI()
 
 
-def animal(name: Union[str, None] = None):
-    if name:
-        return name
-    else:
-        return "default"
+# 父依赖项
+def animal(name: str = Form()):
+    print(name)
+    return name
 
 
+# 子依赖项
 def cat(name: str = Depends(animal), foot: int = 4):
-    return f"animal is {name},it has {foot} foot"
+    return f"{name} has {foot} foot"
 
 
 def people(name: str = Depends(animal), foot: int = 2):
-    return f"animal is {name},it has {foot} foot"
+    return f"{name} has {foot} foot"
 
 
 @app.post("/test_cat")
-async def test_function(cat: str = Depends(cat)):
-    print(cat)
-    return cat
+async def test_cat_function(cat_obj: str = Depends(cat)):
+    print(cat_obj)
+    return cat_obj
 
 
 @app.post("/test_people")
-async def test_people_function(people: str = Depends(people)):
-    print(people)
-    return people
+async def test_people_function(people_obj: str = Depends(people)):
+    print(people_obj)
+    return people_obj
